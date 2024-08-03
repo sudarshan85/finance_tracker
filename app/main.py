c199a678c9bd1ba871ea12488de4bd9742291cb2
Sudarshan Srinivasan <sudarshan85@gmail.com>
2024-08-03 10:46:42 -0400

    Done transactions and stores

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.core.config import settings
 from app.db.database import get_db
-from app.api.v1.endpoints import accounts, categories
+from app.api.v1.endpoints import categories, accounts, transactions, stores
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
 
 app.include_router(categories.router, prefix="/api/v1/categories", tags=["categories"])
 app.include_router(accounts.router, prefix="/api/v1/accounts", tags=["accounts"])
+app.include_router(transactions.router, prefix="/api/v1/transactions", tags=["transactions"])
+app.include_router(stores.router, prefix="/api/v1/stores", tags=["stores"])
 
 @app.get("/")
 async def root(db: Session = Depends(get_db)):
